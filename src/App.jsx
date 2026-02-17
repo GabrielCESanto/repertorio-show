aff64c1027ab8005fc04d0bda5866a742d802a1b
Gabriel <gabrielevita@hotmail.com.br>
2026-02-17 00:44:47 -0300

    conectado ao supabase

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,7 @@ function App() {
     try {
       // OBS: no GitHub Pages localhost não funciona.
       // Depois vamos trocar isso pelo endpoint do Supabase.
-      const resp = await fetch("https://XXXXX.supabase.co/functions/v1/pedido", {
+      const resp = await fetch("https://supabase.com/dashboard/project/dkzkutpbtkysebhunjrf/functions", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
